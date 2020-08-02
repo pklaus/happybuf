1d8b32027a51d1ed78b617259f51587d8e4adb43
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2020-08-02 14:33:37 +0200

    tests/addressbook.py -> binary output file name prefix

--- tests/addressbook.py
+++ tests/addressbook.py
@@ -5,22 +5,22 @@ from happybuf import Happybuf
 from happybuf.capnproto import Backend as CapnBackend
 
 hb = Happybuf(schema="schemas/addressbook.capnp", backend=CapnBackend)
-with open("example_capnproto.bin", "w+b") as f:
+with open("addressbook_capnproto.bin", "w+b") as f:
     hb.write(f, "Person", dict(id=1, name="Roland", email="test@example.com"))
     hb.write(f, "Person", dict(id=2, name="Reinhard", email="test@example.com"))
     hb.write(f, "Person", dict(id=3, name="Rydberg", email="test@example.com"))
-with open("example_capnproto.bin", "rb") as f:
+with open("addressbook_capnproto.bin", "rb") as f:
     print(list(hb.read_multiple(f, "Person")))
 
 
 from happybuf.flatbuffers import Backend as FlatBackend
 
 hb = Happybuf(schema="schemas/addressbook.fbs", backend=FlatBackend)
-with open("example_flatbuffers.bin", "w+b") as f:
+with open("addressbook_flatbuffers.bin", "w+b") as f:
     hb.write(f, "address_book.Person.PersonT", dict(id=1, name="Roland", email="test@example.com"))
     hb.write(f, "address_book.Person.PersonT", dict(id=2, name="Reinhard", email="test@example.com"))
     hb.write(f, "address_book.Person.PersonT", dict(id=3, name="Rydberg", email="test@example.com"))
-with open("example_flatbuffers.bin", "rb") as f:
+with open("addressbook_flatbuffers.bin", "rb") as f:
     for person in hb.read_multiple(f, "address_book.Person.Person"):
         print(f"Person: {person.Id()} {person.Name()} {person.Email()}")
 
@@ -28,10 +28,10 @@ with open("example_flatbuffers.bin", "rb") as f:
 from happybuf.protobuf import Backend as ProtoBackend
 
 hb = Happybuf(schema="schemas/addressbook.proto", backend=ProtoBackend)
-with open("example_protobuf.bin", "w+b") as f:
+with open("addressbook_protobuf.bin", "w+b") as f:
     hb.write(f, "Person", dict(id=1, name="Roland", email="test@example.com"))
     hb.write(f, "Person", dict(id=2, name="Reinhard", email="test@example.com"))
     hb.write(f, "Person", dict(id=3, name="Rydberg", email="test@example.com"))
-with open("example_protobuf.bin", "rb") as f:
+with open("addressbook_protobuf.bin", "rb") as f:
     for person in hb.read_multiple(f, "Person"):
         print(f"Person: {person.id=} {person.name=} {person.email=}")
